285c072e2a9dfe4e4608a2d6446c77a2a6370a0c
Zach <zbayard@gmail.com>
2021-01-20 13:30:30 -0500

    core done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// write your code here
+// *****DOM STUFF*****
 const ramenMenu = document.querySelector("#ramen-menu")
 const ramenShow = document.querySelector("#ramen-detail")
 const ramenImg = document.querySelector(".detail-image")
@@ -8,9 +8,9 @@ const ratingForm = document.querySelector("#ramen-rating")
 const ratingLabel = ratingForm.querySelector("label")
 const commentLabel = ratingForm.querySelector("#comment")
 
-console.log(commentLabel)
 
 
+// *****RENDER RAMEN*****
 function renderRamen(ramenObj){
     const ramenPic = document.createElement("img")
     ramenPic.src = `${ramenObj.image}`
@@ -39,65 +39,14 @@ function renderRamen(ramenObj){
         })
     })
 
-    
-
-        ratingForm.addEventListener("submit", e => {
-            e.preventDefault()
-            
-            const ratingInput = e.target.rating.value
-            const commentInput = e.target.comment.value
-    
-            // ratingLabel.innerHTML = `Rating: ${ratingInput}`
-            // commentLabel.innerHTML = `Comment: ${commentInput}`
-            
-
-            fetch(`http://localhost:3000/ramens/${ratingForm.dataset.id}`, {
-            method: 'PATCH',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                rating: ratingInput,
-                comment: commentInput
-            }),
-            })
-            .then(response => response.json())
-            .then(data => {
-                ratingLabel.innerHTML = `Rating: ${data.rating}`
-        
-                commentLabel.innerHTML = `Comment: ${data.comment}`
-                 
-                
-                ratingForm.reset()
-            console.log('Success:', data);
-            })
-            
-    
-    
-        })
-
-
-        
-
-        
-
-
-
-    
-  
-
-
-
 }
 
+// *****FORM EVENT LISTENER/PATCH*****
 ratingForm.addEventListener("submit", e => {
     e.preventDefault()
     
     const ratingInput = e.target.rating.value
     const commentInput = e.target.comment.value
-
-    // ratingLabel.innerHTML = `Rating: ${ratingInput}`
-    // commentLabel.innerHTML = `Comment: ${commentInput}`
     
 
     fetch(`http://localhost:3000/ramens/${ratingForm.dataset.id}`, {
@@ -122,6 +71,7 @@ ratingForm.addEventListener("submit", e => {
     })
 })
 
+// *****INITIAL GET FETCH*****
 fetch(`http://localhost:3000/ramens`)
 .then(response => response.json())
 .then(ramenObj => ramenObj.forEach(ramen => {
@@ -132,9 +82,6 @@ fetch(`http://localhost:3000/ramens`)
 
 
 
-// - Update the rating and comment for a ramen. When the `#ramen-rating` form is submitted, it should update the value on the server. 
-// Changes should also be reflected on the frontend (you can test this by submitting the form; clicking a different ramen image; 
-//     then clicking the image for the ramen you updated - you should see the rating and comment that you submitted previously).
 
 
 
